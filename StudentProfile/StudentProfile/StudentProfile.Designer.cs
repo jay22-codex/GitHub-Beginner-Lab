92b8eb5874e8c44a4d4d73fe0fee9f2760688dd6
B-jay Hernandez <hernandezbjay84@gmail.com>
2026-09-13 23:10:34 +0800

    Update year level to 3

--- StudentProfile/StudentProfile/StudentProfile.Designer.cs
+++ StudentProfile/StudentProfile/StudentProfile.Designer.cs
@@ -31,6 +31,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -63,11 +64,22 @@
             this.label3.TabIndex = 2;
             this.label3.Text = "Email: hernandezbjay84@gmail.com";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(190, 253);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(251, 42);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Year Level: 3";
+            // 
             // StudentProfile
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(701, 244);
+            this.ClientSize = new System.Drawing.Size(701, 318);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -83,6 +95,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
