e98e54a5b5cde8649b843f3aa1c10167d4b9d398
B-jay Hernandez <hernandezbjay84@gmail.com>
2026-09-13 20:18:34 +0800

    Updated

--- StudentProfile/StudentProfile/StudentProfile.Designer.cs
+++ StudentProfile/StudentProfile/StudentProfile.Designer.cs
@@ -46,11 +46,11 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(64, 129);
+            this.label2.Location = new System.Drawing.Point(58, 128);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(567, 42);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Contact Number: 09926422997";
+            this.label2.Text = "Contact Number: 09926422996";
             // 
             // StudentProfile
             // 
